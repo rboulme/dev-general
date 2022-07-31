a4cabf18757e87bac0f42e5d731a55b7ea414250
Rami Boul <rboul71@gmail.com>
2022-07-31 06:45:05 +0300

    remove hemlet and expression libraries

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose'); 
 
 const brands = require('./routes/brands');
+const home = require('./routes/home');
 
 const app = express();
 app.use(express.json());
@@ -12,9 +13,9 @@ mongoose.connect("mongodb+srv://rboul:rraammii@cluster0.cqozq.mongodb.net/?retry
 then(()=>console.log('Connected to MongoDB...')).
 catch(err=>console.error('Could not connect to MongoDB.',err));
 app.use('/api/brands',brands);
+app.use('/api/brands',home);
 
-require('./startup/prod')(app);
-const port = process.env.port || 5000;
+const port = process.env.port || 80;
 app.listen(port,()=>{
     console.log(`Listening on port ${port}` );
 })
